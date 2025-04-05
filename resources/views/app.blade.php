e444de14bcc16f1275a696c42beb10b442e97bc2
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-05 12:46:17 +0100

    {{-- 02-Inline style to set the HTML background color based on our theme in app.css --}}

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,7 +19,7 @@
             })();
         </script>
 
-        {{-- Inline style to set the HTML background color based on our theme in app.css --}}
+        {{-- 02-Inline style to set the HTML background color based on our theme in app.css --}}
         <style>
             html {
                 background-color: oklch(1 0 0);
