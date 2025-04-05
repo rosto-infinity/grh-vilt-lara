55b0c2d991b3ed684664cf0d1854f15bfa55586c
ROSTAND LELE <rostoinfocus@gmail.com>
2025-04-05 12:46:08 +0100

    {{-- 02-Inline script to detect system dark mode preference and apply it immediately --}}

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- Inline script to detect system dark mode preference and apply it immediately --}}
+        {{-- 02-Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
             (function() {
                 const appearance = '{{ $appearance ?? "system" }}';
